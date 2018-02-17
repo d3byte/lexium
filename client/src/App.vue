677c897b0c289230a4349b226b944b3c12d4c497
d3byte <pandinator@protonmail.com>
2018-02-17 18:42:31 +0300

    Минорные изменения

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,33 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/signin">Signin</router-link> |
-      <router-link to="/signup">Signup</router-link>
-    </div>
+    <Header/>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+    components: {
+        Header,
+    }
 }
 
-#nav {
-  padding: 30px;
-}
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="sass"> 
 </style>
