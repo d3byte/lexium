2d5684076a2eb7cc75cf5f2ae6264a0d592761d6
d3byte <pandinator@protonmail.com>
2018-03-02 22:59:48 +0300

    Переделал страницу 404 в функциональный компонент

--- app/src/pages/Error404/Error404.jsx
+++ app/src/pages/Error404/Error404.jsx
@@ -1,29 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 import './style.css'
 
-export default class Error404 extends Component {
-  render() {
-    return (
-      <div className="main">
-        <div class="wrapper">
-        <div class="title">Ошибка 404</div>
-        <div class="sub-title">К сожалению, такой страницы нет :(</div>
-        <div class="proposal">Может вы искали одну из этих страниц?</div>
-          <ul class="links">
-              <li>
-                  <Link to="/signin">Авторизация</Link>
-              </li> |
-              <li>
-                  <Link to="/signup">Регистрация</Link>
-              </li> |
-              <li>
-                  <Link to="/profile">Профиль</Link>
-              </li>
-          </ul>
-        </div>
-      </div>
-    )
-  }
-}
+export const Error404 = () => (
+  <div className="main">
+    <div class="wrapper">
+    <div class="title">Ошибка 404</div>
+    <div class="sub-title">К сожалению, такой страницы нет :(</div>
+    <div class="proposal">Может вы искали одну из этих страниц?</div>
+      <ul class="links">
+          <li>
+              <Link to="/signin">Авторизация</Link>
+          </li> |
+          <li>
+              <Link to="/signup">Регистрация</Link>
+          </li> |
+          <li>
+              <Link to="/profile">Профиль</Link>
+          </li>
+      </ul>
+    </div>
+  </div>
+)
