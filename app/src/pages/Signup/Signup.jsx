b0b2da30ae373f56e26a8bca84f54e11aed8c971
d3byte <pandinator@protonmail.com>
2018-03-02 23:08:19 +0300

    Страница реги

--- app/src/pages/Signup/Signup.jsx
+++ app/src/pages/Signup/Signup.jsx
@@ -2,13 +2,59 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { graphql } from 'react-apollo'
 
+import Button from '../../components/Button'
+import Header from '../../components/Header'
+
+import arrow from '../../assets/arrow.svg'
+
 import { SIGNUP } from '../graphql/mutations'
 
 class Signup extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loading: false,
+      firstName: '',
+      lastName: '',
+      username: '',
+      password: ''
+    }
+  }
+
+  inputHandler(e, target) {
+    this.setState({
+      [target]: e.target.value
+    })
+  }
+
+  submit = async () => {
+    // Отправить форму
+
+  }
+
   render() {
+    const { loading } = this.state
     return (
       <div>
-
+        <div className={'home ' + (loading && 'hide')}>
+          <Header/>
+          <div className="cards">
+              <div className="card rounded">
+                  <div className="card-header">Регистрация</div>
+                  <div className="card-body">
+                      <input type="text" onChange={e => this.inputHandler(e, 'firstName')} className="line-based" placeholder="Имя"/>
+                      <input type="text" onChange={e => this.inputHandler(e, 'lastName')} className="line-based" placeholder="Фамилия"/>
+                  </div>
+                  <div className="card-footer">
+                      <Button clickHandler={this.submit} classNameProp="authorization" text="Войти" />
+                  </div>
+              </div>
+              <Link className="form-link" to="/signin">Есть аккаунт? <img src={arrow}/></Link>
+          </div>
+      </div>
+      {
+        loading && <Loading />
+      }
       </div>
     )
   }
