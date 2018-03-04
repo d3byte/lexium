be56c22f2952707f75f995661fa649b1b742d007
d3byte <pandinator@protonmail.com>
2018-03-04 14:10:25 +0300

    caching

--- app/src/graphql/queries.js
+++ app/src/graphql/queries.js
@@ -1,3 +1,42 @@
 import gql from 'graphql-tag'
 
-export default {}
+const USER = gql`
+query user {
+    user {
+        id
+        name
+        username
+        email
+        wordsLearnt
+        groups {
+            id
+            name
+            tasks {
+                id
+                name
+                words
+                results {
+                    id
+                    wordsLearnt
+                    user {
+                        id
+                        name
+                        wordsLearnt
+                    }
+                    percentage
+                }
+                deadline
+                createdAt
+            }
+            superUsers
+            createdAt
+        }
+        avatarUrl
+        createdAt
+    }
+}
+`
+
+export {
+    USER,
+}
