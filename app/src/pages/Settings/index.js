714d434368180afe412fc47898fdeaf3be501209
Sergey Savtyra <pandinator@protonmail.com>
2018-03-13 00:22:13 +0300

    Сверстал форму загрузки аватарки

--- app/src/pages/Settings/index.js
+++ app/src/pages/Settings/index.js
@@ -1,11 +1,116 @@
 import React, { Component } from 'react'
+import { withApollo } from 'react-apollo'
 
-export default class Settings extends Component {
+import Button from '../../components/Button'
+import Header from '../../components/Header'
+
+import './style.css'
+
+import { CacheManager } from '../../utils/index'
+import { USER } from '../../graphql/queries'
+import { UPDATE_AVATAR } from '../../graphql/mutations'
+
+class Settings extends Component {
+  constructor() {
+    super()
+    this.state = {
+      image: '',
+      user: {},
+      fetching: false,
+      updatedAvatar: false,
+    }
+    this.cache = new CacheManager()
+    this.client = {}
+    this.token = ''
+  }
+
+  uploadImage = e => {
+    const file = e.target.files[0]
+    if (!file) {
+      return
+    }
+    this.setState({ fetching: true })
+    const reader = new FileReader()
+    reader.onload = async e => {
+      this.setState({ image: e.target.result })
+      const user = await this.client.mutate({
+        mutation: UPDATE_AVATAR, 
+        variables: { token: this.token, avatarUrl: e.target.result } 
+      })
+      this.cache.writeData('user', user)
+      this.setState({ fetching: false, updatedAvatar: true, savingAvatar: false})
+    }
+    reader.readAsDataURL(file)
+  }
+
+
+  fetchData = async token => {
+    const response = await this.client.query({ query: USER, variables: { token } })
+    const { user } = response.data
+    this.cache.writeData('user', user)
+    this.setState({ user, fetching: false })
+  }
+
+  componentDidMount = async () => {
+    const { client } = this.props
+    this.client = client
+    try {
+      const cachedUser = await this.cache.readData('user')
+      const token = await this.cache.readData('token')
+      this.token = token
+      this.setState({ user: cachedUser, fetching: true })
+      this.fetchData(token)
+    } catch (error) {
+      console.log(error)
+      this.props.history.push('/signin')
+    }
+  }
+
   render() {
+    const { user, fetching, image, updatedAvatar } = this.state
+    const { pathname } = this.props.location
+    const { history } = this.props
     return (
-      <div>
+      <div className="settings">
+        <Header fetching={fetching} pathname={pathname} history={history} inputHandler={this.searchTasks} />
         
+        <div className="section">
+          <span className="">Аккаунт</span>
+          <div className="containers">
+
+            <div className="container-settings">
+              <div className="avatar">
+                {
+                  updatedAvatar && <span className="loading-status">Изображение загружено</span>
+                }
+                <input type="file" onChange={this.uploadImage} name="image" accept="image/*" />
+                {
+                  user.avatarUrl || image ? 
+                    <img src={user.avatarUrl || image} alt="user-avatar" /> :
+                    <i className="material-icons">file_upload</i>
+                }
+              </div>
+              <div className="container-main">
+                <div className="form">
+                  <div className="form-group">
+                    <label>Логин</label>
+                    <input type="text" className="line-based" placeholder={user && user.username} />
+                  </div>
+                  <div className="form-group">
+                    <label>Почта</label>
+                    <input type="text" className="line-based" placeholder={user && user.email} />
+                  </div>
+                  <Button clickHandler={() => console.log('Ура!')} classNameProp="regular lighter" text="Сохранить" />
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
       </div>
     )
   }
 }
+
+export default withApollo(Settings)
