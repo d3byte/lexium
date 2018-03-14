91ad9ceb867640bf8aa0dda3a8e956b39b4fc445
Sergey Savtyra <pandinator@protonmail.com>
2018-03-14 23:26:23 +0300

    Создал папку со скрэмблом и добавил раут соответствующий

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -16,6 +16,7 @@ import FlashCards from './pages/FlashCards'
 import FindPair from './pages/FindPair'
 import Typein from './pages/Typein'
 import Settings from './pages/Settings'
+import Scramble from './pages/Scramble'
 
 export default class App extends Component {
   componentWillReceiveProps = props => {
@@ -35,6 +36,7 @@ export default class App extends Component {
             <Route exact path="/task/learn" component={FlashCards} />
             <Route exact path="/task/find" component={FindPair} />
             <Route exact path="/task/typein" component={Typein} />
+            <Route exact path="/task/typein" component={Scramble} />
             <Route exact path="/settings" component={Settings} />
             <Route exact path="*" component={Error404} />
           </Switch>
