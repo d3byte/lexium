12e741cb6a8a8b1af67db22a8341a603b6e18cc3
Sergey Savtyra <pandinator@protonmail.com>
2018-03-17 19:24:02 +0300

    Вёрстка теста

---
+++ app/src/pages/Test/index.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+import Button from '../../components/Button'
+import Header from '../../components/Header'
+import { RadioButton } from '../../components/RadioButton'
+
+import { CacheManager } from '../../utils'
+
+import './style.css'
+
+export default class Typein extends Component {
+  constructor() {
+    super()
+    this.state = {}
+    this.cache = new CacheManager()
+  }
+
+  componentDidMount = () => {
+  }
+
+  render() {
+    const { history } = this.props
+    const { pathname } = this.props.location
+    const { currentWord, splittedWord, currentKey, correct, currentLetter } = this.state
+    return (
+      <div className="task-game">
+        <Header fetching={false} pathname={pathname} history={history} />
+
+        <div className="section">
+          <Link className="return-link" to="/task">
+            <i className="material-icons">arrow_back</i> Вернуться
+          </Link>
+          <div className="test-title">Тест</div>
+
+          <div className="containers">
+            <div className="container">
+              <span className="title">Перевод</span>
+              <div className="word-group">
+                <label>Banana</label>
+                <input type="text" className="line-based" placeholder="Перевод" />
+              </div>
+              <div className="word-group">
+                <label>Allergy</label>
+                <input type="text" className="line-based" placeholder="Перевод" />
+              </div>
+            </div>
+
+            <div className="container">
+              <span className="title right">Выбор значения</span>
+              <div className="word-group">
+                <label>Манго</label>
+                <div className="variants">
+                  <RadioButton label="Mango" name="Манго" value="Mango" />
+                  <RadioButton label="Allergy" name="Манго" value="Allergy" />
+                  <RadioButton label="KFC" name="Манго" value="KFC" />
+                </div>
+              </div>
+              <div className="word-group">
+                <label>Аллергия</label>
+                <div className="variants">
+                  <RadioButton label="Mango" name="Манго" value="Cucumber" />
+                  <RadioButton label="Allergy" name="Манго" value="Allergy" />
+                  <RadioButton label="KFC" name="Манго" value="Chicken" />
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <center>
+            <Button clickHandler={() => console.log('Ура!')} classNameProp="regular lighter" text="Отправить" />
+          </center>
+
+        </div>
+
+      </div>
+    )
+  }
+}
