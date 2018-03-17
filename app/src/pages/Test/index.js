e57ce4ebf6fd8cb319d311a0149aad1ba0990ba2
Sergey Savtyra <pandinator@protonmail.com>
2018-03-17 22:59:58 +0300

    Сверстал экран результата после теста

--- app/src/pages/Test/index.js
+++ app/src/pages/Test/index.js
@@ -9,10 +9,12 @@ import { CacheManager } from '../../utils'
 
 import './style.css'
 
-export default class Typein extends Component {
+export default class Test extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      completed: true,
+    }
     this.cache = new CacheManager()
   }
 
@@ -22,57 +24,69 @@ export default class Typein extends Component {
   render() {
     const { history } = this.props
     const { pathname } = this.props.location
-    const { currentWord, splittedWord, currentKey, correct, currentLetter } = this.state
+    const { currentWord, splittedWord, currentKey, correct, currentLetter, completed } = this.state
     return (
       <div className="task-game">
         <Header fetching={false} pathname={pathname} history={history} />
+        {
+          !completed ? (
+            <div className="section">
+              <Link className="return-link" to="/task">
+                <i className="material-icons">arrow_back</i> Вернуться
+              </Link>
+              <div className="test-title">Тест</div>
 
-        <div className="section">
-          <Link className="return-link" to="/task">
-            <i className="material-icons">arrow_back</i> Вернуться
-          </Link>
-          <div className="test-title">Тест</div>
-
-          <div className="containers">
-            <div className="container">
-              <span className="title">Перевод</span>
-              <div className="word-group">
-                <label>Banana</label>
-                <input type="text" className="line-based" placeholder="Перевод" />
-              </div>
-              <div className="word-group">
-                <label>Allergy</label>
-                <input type="text" className="line-based" placeholder="Перевод" />
-              </div>
-            </div>
-
-            <div className="container">
-              <span className="title right">Выбор значения</span>
-              <div className="word-group">
-                <label>Манго</label>
-                <div className="variants">
-                  <RadioButton label="Mango" name="Манго" value="Mango" />
-                  <RadioButton label="Allergy" name="Манго" value="Allergy" />
-                  <RadioButton label="KFC" name="Манго" value="KFC" />
+              <div className="containers">
+                <div className="container">
+                  <span className="title">Перевод</span>
+                  <div className="word-group">
+                    <label>Banana</label>
+                    <input type="text" className="line-based" placeholder="Перевод" />
+                  </div>
+                  <div className="word-group">
+                    <label>Allergy</label>
+                    <input type="text" className="line-based" placeholder="Перевод" />
+                  </div>
                 </div>
-              </div>
-              <div className="word-group">
-                <label>Аллергия</label>
-                <div className="variants">
-                  <RadioButton label="Mango" name="Аллергия" value="Cucumber" />
-                  <RadioButton label="Allergy" name="Аллергия" value="Allergy" />
-                  <RadioButton label="KFC" name="Аллергия" value="Chicken" />
+
+                <div className="container">
+                  <span className="title right">Выбор значения</span>
+                  <div className="word-group">
+                    <label>Манго</label>
+                    <div className="variants">
+                      <RadioButton label="Mango" name="Манго" value="Mango" />
+                      <RadioButton label="Allergy" name="Манго" value="Allergy" />
+                      <RadioButton label="KFC" name="Манго" value="KFC" />
+                    </div>
+                  </div>
+                  <div className="word-group">
+                    <label>Аллергия</label>
+                    <div className="variants">
+                      <RadioButton label="Mango" name="Аллергия" value="Cucumber" />
+                      <RadioButton label="Allergy" name="Аллергия" value="Allergy" />
+                      <RadioButton label="KFC" name="Аллергия" value="Chicken" />
+                    </div>
+                  </div>
                 </div>
+
               </div>
+
+              <center>
+                <Button clickHandler={() => console.log('Ура!')} classNameProp="regular lighter" text="Отправить" />
+              </center>
+
             </div>
-
-          </div>
-
-          <center>
-            <Button clickHandler={() => console.log('Ура!')} classNameProp="regular lighter" text="Отправить" />
-          </center>
-
-        </div>
+          ) : (
+            <div className="result">
+              <div className="circle">
+                <i className="material-icons">done_all</i>
+              </div>
+              <p>Вы прошли тест!</p>
+              <p>Ваш результат: 36%</p>
+              <Button clickHandler={() => console.log('Ура!')} classNameProp="regular lighter" text="Вернуться" />
+            </div>
+          )
+        }
 
       </div>
     )
