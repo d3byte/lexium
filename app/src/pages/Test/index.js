8095ca1cfcb778544fd738934c9b362020cfb1e3
Sergey Savtyra <pandinator@protonmail.com>
2018-03-17 23:08:08 +0300

    Небольшие фиксики

--- app/src/pages/Test/index.js
+++ app/src/pages/Test/index.js
@@ -13,7 +13,7 @@ export default class Test extends Component {
   constructor() {
     super()
     this.state = {
-      completed: true,
+      completed: false,
     }
     this.cache = new CacheManager()
   }
