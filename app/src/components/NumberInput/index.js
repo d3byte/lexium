74e46d3af24819f728aa7ce12f218b5602cbd81d
Sergey Savtyra <pandinator@protonmail.com>
2018-03-18 16:53:06 +0300

    Компонент NumberInput

---
+++ app/src/components/NumberInput/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style.css'
+
+export const NumberInput = () => (
+    <div className="number-input">
+        <div className="controller">-</div>
+        <span className="value">4</span>
+        <div className="controller">+</div>
+    </div>
+)
