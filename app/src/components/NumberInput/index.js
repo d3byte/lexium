82112d2bddfd36516996cebc1043e0d1a28db1ad
Sergey Savtyra <pandinator@protonmail.com>
2018-03-18 21:34:11 +0300

    Компонент NumberInput теперь полноценно работает

--- app/src/components/NumberInput/index.js
+++ app/src/components/NumberInput/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 import './style.css'
 
-export const NumberInput = () => (
+export const NumberInput = ({ value, field, handler }) => (
     <div className="number-input">
-        <div className="controller">-</div>
-        <span className="value">4</span>
-        <div className="controller">+</div>
+        <div className="controller" onClick={() => handler(value - 1, field)}>-</div>
+        <span className="value">{value}</span>
+        <div className="controller" onClick={() => handler(value + 1, field)}>+</div>
     </div>
 )
