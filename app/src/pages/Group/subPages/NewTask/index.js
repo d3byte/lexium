cbc4871707f1aa8e68b03bbcb6c566673b458b84
Sergey Savtyra <pandinator@protonmail.com>
2018-03-24 21:32:30 +0300

    Улучшил оповещение об успешном создании задания

--- app/src/pages/Group/subPages/NewTask/index.js
+++ app/src/pages/Group/subPages/NewTask/index.js
@@ -98,7 +98,8 @@ export default class NewTask extends Component {
         id: 0,
         key: '',
         value: ''
-      }]
+      }],
+      success: false
     })
   }
 
@@ -123,6 +124,7 @@ export default class NewTask extends Component {
     if (month < 10) {
       month = '0' + month
     }
+    this.setState({ deadline: `${year}-${month}-${day}` })
     const deadlineDate = `${year}-${month}-${day}T00:00:00.480Z`
     try {
       const response = await this.client.mutate({ 
@@ -144,7 +146,6 @@ export default class NewTask extends Component {
         }
       })
       this.cache.writeData('user', user).then(() => {
-        this.nullFields()
         this.setState({ success: true })
       })
     } catch (error) {
@@ -159,96 +160,119 @@ export default class NewTask extends Component {
   }
 
   render() {
-    const { learnWords, findPair, typeIn, scramble, wordPairs, success } = this.state
+    const { 
+      learnWords, findPair, typeIn, scramble, wordPairs, success,
+      name, deadline
+    } = this.state
     return (
       <form onSubmit={e => {
         e.preventDefault()
         this.submit()
       }} className="section group-subpage">
-        <div className="left containers">
-          <div className="container">
-            <span className="title">Информация о задании</span>
-            <div className="word-group">
-              <label>Как назвать задание?</label>
-              <input 
-                type="text" className="line-based" 
-                placeholder="Название" name="name" 
-                onChange={this.infoInput} required
-              />
-            </div>
-            <div className="word-group">
-              <label>Сколько дней на выполнение?</label>
-              <input 
-                type="number" className="line-based" 
-                placeholder="Дни" name="deadline"
-                min="1"
-                onChange={this.infoInput} required
-              />
-            </div>
-          </div>
-
-          <div className="container attempts">
-            <span className="title">Количество повторений</span>
-            <div className="attempt-input">
-              <span className="name">Выучи слова</span>
-              <NumberInput handler={this.attemptInput} value={learnWords} field="learnWords" />
-            </div>
-            <div className="attempt-input">
-              <span className="name">Найди пару</span>
-              <NumberInput handler={this.attemptInput} value={findPair} field="findPair" />
-            </div>
-            <div className="attempt-input">
-              <span className="name">Введи слово</span>
-              <NumberInput handler={this.attemptInput} value={typeIn} field="typeIn" />
-            </div>
-            <div className="attempt-input">
-              <span className="name">Скрэмбл</span>
-              <NumberInput handler={this.attemptInput} value={scramble} field="scramble" />
-            </div>
-          </div>
-
-        </div>
-        
-        <div className="right containers">
-          <div className="container editor">
-            <span className="title right">Редактор слов</span>
-            <div className="word-editor">
-              <div className="word-table">
-                <div className="table-row">
-                  <div className="cell edit-heading">Слово</div>
-                  <div className="cell edit-heading">Перевод</div>
+        {
+          !success && (
+            <div className="left containers">
+              <div className="container">
+                <span className="title">Информация о задании</span>
+                <div className="word-group">
+                  <label>Как назвать задание?</label>
+                  <input 
+                    type="text" className="line-based" 
+                    placeholder="Название" name="name" 
+                    onChange={this.infoInput} required
+                  />
                 </div>
-                {
-                  wordPairs.map((item, index) => (
-                    <div className="table-row" key={item.id}>
-                      <div className="cell">
-                        <input 
-                          type="text" placeholder="Banana" 
-                          name="key" onChange={e => this.editWordPair(e, item)}
-                          onKeyDown={e => this.removeWordPair(e, item, index)}
-                          ref={ref => this.inputTofocus = ref}
-                          required
-                        />
-                      </div>
-                      <div className="cell">
-                        <input 
-                          type="text" placeholder="Банан" 
-                          name="value" onChange={e => this.editWordPair(e, item)}
-                          onKeyDown={e => this.hotKeyAddWordPair(e, index)}
-                          required
-                        />
-                      </div>
-                    </div>
-                  ))
-                }
+                <div className="word-group">
+                  <label>Сколько дней на выполнение?</label>
+                  <input 
+                    type="number" className="line-based" 
+                    placeholder="Дни" name="deadline"
+                    min="1"
+                    onChange={this.infoInput} required
+                  />
+                </div>
               </div>
-              <button className="regular add-words" onClick={this.addWordPair}>+</button>
-              <button className="regular save-words">Создать</button>
-              { success && <p className="success-msg">Задание успешно создано</p> }
-            </div>
 
-          </div>
-        </div>
+              <div className="container attempts">
+                <span className="title">Количество повторений</span>
+                <div className="attempt-input">
+                  <span className="name">Выучи слова</span>
+                  <NumberInput handler={this.attemptInput} value={learnWords} field="learnWords" />
+                </div>
+                <div className="attempt-input">
+                  <span className="name">Найди пару</span>
+                  <NumberInput handler={this.attemptInput} value={findPair} field="findPair" />
+                </div>
+                <div className="attempt-input">
+                  <span className="name">Введи слово</span>
+                  <NumberInput handler={this.attemptInput} value={typeIn} field="typeIn" />
+                </div>
+                <div className="attempt-input">
+                  <span className="name">Скрэмбл</span>
+                  <NumberInput handler={this.attemptInput} value={scramble} field="scramble" />
+                </div>
+              </div>
+
+            </div>
+          )
+        }
+        {
+          !success && (
+            <div className="right containers">
+              <div className="container editor">
+                <span className="title right">Редактор слов</span>
+                <div className="word-editor">
+                  <div className="word-table">
+                    <div className="table-row">
+                      <div className="cell edit-heading">Слово</div>
+                      <div className="cell edit-heading">Перевод</div>
+                    </div>
+                    {
+                      wordPairs.map((item, index) => (
+                        <div className="table-row" key={item.id}>
+                          <div className="cell">
+                            <input 
+                              type="text" placeholder="Banana" 
+                              name="key" onChange={e => this.editWordPair(e, item)}
+                              onKeyDown={e => this.removeWordPair(e, item, index)}
+                              ref={ref => this.inputTofocus = ref}
+                              required
+                            />
+                          </div>
+                          <div className="cell">
+                            <input 
+                              type="text" placeholder="Банан" 
+                              name="value" onChange={e => this.editWordPair(e, item)}
+                              onKeyDown={e => this.hotKeyAddWordPair(e, index)}
+                              required
+                            />
+                          </div>
+                        </div>
+                      ))
+                    }
+                  </div>
+                  <button className="regular add-words" onClick={this.addWordPair}>+</button>
+                  <button className="regular save-words">Создать</button>
+                  { success && <p className="success-msg">Задание успешно создано</p> }
+                </div>
+
+              </div>
+            </div>
+          )
+        }
+        {
+          success && (
+            <div className="successful-process containers">
+              <p>Задание успешно создано!</p>
+              <div className="container">
+                <p className="task-name">{name}</p>
+                <p className="task-wordpairs">Пар слов: <b>{wordPairs && wordPairs.length}</b></p>
+                <p className="task-deadline">Выполнить до <b>{deadline}</b></p>
+              </div>
+              <button className="regular lighter" onClick={this.nullFields}>Создать ещё</button>
+            </div>
+          )
+        }
 
       </form>
     )
