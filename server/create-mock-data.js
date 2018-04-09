1b7e819f42a22bf79bd731a2d35fc28a9ff4b9a6
Sergey Savtyra <pandinator@protonmail.com>
2018-04-09 11:02:12 +0300

    Убрал создание mock data

--- server/create-mock-data.js
+++ server/create-mock-data.js
@@ -1,39 +1,24 @@
 const { models, sequelize } = require('./models')
 
-function createData () {
-  let groupsPromise = models.Group.bulkCreate([
-    {
-      name: 'Лежать+сосать',
-    },
-    {
-      name: 'Кто это тут сосёт?',
-    }
-  ])
+// function createData () {
+//   Promise.all([groupsPromise])
+//     .then(() => Promise.all([
+//       models.Group.findAll()
+//     ]))
+//     .then(function ([groups]) {
+//       let promises = []
+//       promises.push(groups)
+//       promises.push(events[0].setRoom(rooms[0]))
+//       promises.push(events[1].setRoom(rooms[1]))
+//       promises.push(events[2].setRoom(rooms[2]))
 
-  // const HOUR = 60 * 60 * 1000
-  // let now = new Date()
-  // let oneHourLater = new Date(now.getTime() + HOUR)
-  // let twoHoursLater = new Date(oneHourLater.getTime() + HOUR)
-  // let threeHoursLater = new Date(twoHoursLater.getTime() + HOUR)
+//       promises.push(events[0].setUsers([users[0], users[1]]))
+//       promises.push(events[1].setUsers([users[1], users[2]]))
+//       promises.push(events[2].setUsers([users[0], users[2]]))
 
-  Promise.all([groupsPromise])
-    .then(() => Promise.all([
-      models.Group.findAll()
-    ]))
-    .then(function ([groups]) {
-      let promises = []
-      promises.push(groups)
-      // promises.push(events[0].setRoom(rooms[0]))
-      // promises.push(events[1].setRoom(rooms[1]))
-      // promises.push(events[2].setRoom(rooms[2]))
-
-      // promises.push(events[0].setUsers([users[0], users[1]]))
-      // promises.push(events[1].setUsers([users[1], users[2]]))
-      // promises.push(events[2].setUsers([users[0], users[2]]))
-
-      return Promise.all(promises)
-    })
-}
+//       return Promise.all(promises)
+//     })
+// }
 
 sequelize.sync()
-  .then(createData)
+  // .then(createData)
