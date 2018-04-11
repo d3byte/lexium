93f752776bf9cd5ddfc0a095b2a3ca0c3b23be73
Sergey Savtyra <pandinator@protonmail.com>
2018-04-11 23:28:36 +0300

    Осуществил логику выбора слов, неправильных слов и подсветку правильных в FindPair

--- app/src/pages/FindPair/index.js
+++ app/src/pages/FindPair/index.js
@@ -5,7 +5,7 @@ import Button from '../../components/Button'
 import Header from '../../components/Header'
 import { Gamebar } from '../../components/Gamebar'
 
-import { CacheManager } from '../../utils'
+import { CacheManager, shuffle } from '../../utils'
 
 import './style.css'
 
@@ -14,15 +14,69 @@ export default class FindPair extends Component {
     super(props)
     this.state = {
       task: props.location.state.task,
-      takenAttempts: props.location.state.takenAttempts
+      takenAttempts: props.location.state.takenAttempts,
+      words: [],
+      correct: [],
+      incorrect: [],
+      highlighted: [],
+      selected: []
     }
     this.cache = new CacheManager()
+  }
+
+  checkPair = selected => {
+    if (selected[0].pairId === selected[1].pairId) {
+      let correct = this.state.correct.slice(0)
+      correct.push(selected[0].id, selected[1].id)
+      this.setState({ selected: [], highlighted: [], correct })
+      return
+    }
+    this.setState({ 
+      selected: [], highlighted: [], 
+      incorrect: [selected[0].id, selected[1].id]
+    })
+  }
+
+  selectWord = word => {
+    const { correct } = this.state
+    if (correct.includes(word.id))
+      return
+    let selected = this.state.selected.slice(0),
+      highlighted = this.state.highlighted.slice(0)
+    if (highlighted.length === 1) {
+      selected.push(word)
+      this.checkPair(selected)
+      return
+    }
+    selected.push(word)
+    highlighted.push(word.id)
+    this.setState({ selected, highlighted, incorrect: [] })
+  }
+
+  componentDidMount = () => {
+    const { task } = this.state
+    let words = [], index = 0
+    task.words.map(pair => {
+      const key = {
+        pairId: pair.id,
+        id: ++index,
+        value: pair.key
+      }
+      const value = {
+        pairId: pair.id,
+        id: ++index,
+        value: pair.value
+      }
+      words.push(key, value)
+      return pair
+    })
+    this.setState({ words: shuffle(words) })
   }
 
   render() {
     const { history } = this.props
     const { pathname } = this.props.location
-    const { task, takenAttempts } = this.state
+    const { task, takenAttempts, words, selected, correct, incorrect, highlighted } = this.state
     return (
       <div className="task-game">
         <Header fetching={false} pathname={pathname} history={history} />
@@ -41,12 +95,21 @@ export default class FindPair extends Component {
           <span className="title">Выбор пар</span>
 
             <div className="game-wrapper find-pair">
-              <div className="word correct">Banana</div>
-              <div className="word incorrect">Аллергия</div>
-              <div className="word selected">Allergy</div>
-              <div className="word incorrect">Font</div>
-              <div className="word">Шрифт</div>
-              <div className="word correct">Банан</div>
+              {
+                words.map(word => (
+                  <div 
+                    className={
+                      'word ' + (highlighted.includes(word.id) ? 'selected ' : '') +  
+                      (incorrect.includes(word.id) ? 'incorrect ' : '') +
+                      (correct.includes(word.id) ? 'correct ' : '')
+                    }
+                    onClick={() => this.selectWord(word)}
+                    key={word.id}
+                  >
+                    {word.value}
+                  </div>
+                ))
+              }
             </div>
 
 
