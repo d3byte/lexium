45deffdcd89ca4ccc781e3dc9d6b8de3543aa4c2
Sergey Savtyra <pandinator@protonmail.com>
2018-04-11 22:44:17 +0300

    Интегрировал компонент геймбара в Task и FindPair

--- app/src/pages/FindPair/index.js
+++ app/src/pages/FindPair/index.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 
 import Button from '../../components/Button'
 import Header from '../../components/Header'
+import { Gamebar } from '../../components/Gamebar'
 
 import { CacheManager } from '../../utils'
 
 import './style.css'
 
 export default class FindPair extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
+      task: props.location.state.task,
+      takenAttempts: props.location.state.takenAttempts
     }
     this.cache = new CacheManager()
   }
@@ -19,7 +21,7 @@ export default class FindPair extends Component {
   render() {
     const { history } = this.props
     const { pathname } = this.props.location
-    const { showKey } = this.state
+    const { task, takenAttempts } = this.state
     return (
       <div className="task-game">
         <Header fetching={false} pathname={pathname} history={history} />
@@ -31,43 +33,8 @@ export default class FindPair extends Component {
             </span>
             <span className="title">Игры</span>
           </div>
-
-          <div className="game-bar">
-            <Link to="/task/learn" className="game">
-              <span className="name">Выучи слова</span>
-              <span className="attempts">Пройдено <b>1/3</b></span>
-              <div className="hint">
-                <p>В этой игре вам предстоит познакомиться со словами, пользуясь карточками. Вы можете перевернуть карточку,
-                кликнув на неё, таким образом показав аналог на другом
-                языке</p>
-              </div>
-            </Link>
-            <Link to="/task/find" className="game active">
-              <span className="name">Найди пару</span>
-              <span className="attempts">Пройдено <b>1/3</b></span>
-              <div className="hint">
-                <p>В этой игре вам нужно искать слово на одном языке и его аналог на другом.
-                Просто кликайте на карточку со словом и потом на карточку с предположительной парой</p>
-              </div>
-            </Link>
-            <Link to="/task/typein" className="game">
-              <span className="name">Введи слово</span>
-              <span className="attempts">Пройдено <b>1/3</b></span>
-              <div className="hint">
-                <p>Введите эквивалент предложенного слова на другом языке</p>
-              </div>
-            </Link>
-            <Link to="/task/scramble" className="game">
-              <span className="name">Скрэмбл</span>
-              <span className="attempts">Пройдено <b>1/3</b></span>
-              <div className="hint">
-                <p>Составляйте целые слова из букв, разбросанных в случайном порядке. Просто перетягивайте
-                части слова на подходящее, по вашему мнению, место и проверьте ваши знания</p>
-              </div>
-            </Link>
+          <Gamebar task={task} currentTab="find" attempts={task.attempts} takenAttempts={takenAttempts} />
           </div>
-
-        </div>
 
         <div className="section">
           <span className="title">Выбор пар</span>
--- app/src/pages/Task/index.js
+++ app/src/pages/Task/index.js
@@ -4,6 +4,7 @@ import { withApollo } from 'react-apollo'
 
 import Button from '../../components/Button'
 import Header from '../../components/Header'
+import { Gamebar } from '../../components/Gamebar'
 
 import { CacheManager } from '../../utils/index'
 import { USER } from '../../graphql/queries'
@@ -114,40 +115,7 @@ export default class componentName extends Component {
 
         <div className="section">
           <span className="title">Игры</span>
-          <div className="game-bar">
-            <Link to={{ pathname: '/task/learn', state: { task } }} className="game">
-              <span className="name">Выучи слова</span>
-              <span className="attempts">Пройдено <b>{ takenAttempts.learnWords + '/' + (task.attempts && task.attempts.learnWords) }</b></span>
-              <div className="hint">
-                <p>В этой игре вам предстоит познакомиться со словами, пользуясь карточками. Вы можете перевернуть карточку,
-                кликнув на неё, таким образом показав аналог на другом
-                языке</p>
-              </div>
-            </Link>
-            <Link to={{ pathname: '/task/find', state: { task } }} className="game">
-              <span className="name">Найди пару</span>
-              <span className="attempts">Пройдено <b>{ takenAttempts.findPair + '/' + (task.attempts && task.attempts.findPair) }</b></span>
-              <div className="hint">
-                <p>В этой игре вам нужно искать слово на одном языке и его аналог на другом.
-                Просто кликайте на карточку со словом и потом на карточку с предположительной парой</p>
-              </div>
-            </Link>
-            <Link to={{ pathname: '/task/typein', state: { task } }} className="game">
-              <span className="name">Введи слово</span>
-              <span className="attempts">Пройдено <b>{ takenAttempts.typeIn + '/' + (task.attempts && task.attempts.typeIn) }</b></span>
-              <div className="hint">
-                <p>Введите эквивалент предложенного слова на другом языке</p>
-              </div>
-            </Link>
-            <Link to={{ pathname: '/task/scramble', state: { task } }} className="game">
-              <span className="name">Скрэмбл</span>
-              <span className="attempts">Пройдено <b>{ takenAttempts.scramble + '/' + (task.attempts && task.attempts.learnWords) }</b></span>
-              <div className="hint">
-                <p>Составляйте целые слова из букв, разбросанных в случайном порядке. Просто перетягивайте
-                части слова на подходящее, по вашему мнению, место и проверьте ваши знания</p>
-              </div>
-            </Link>
-          </div>
+          <Gamebar task={task} attempts={task.attempts} takenAttempts={takenAttempts}/>
           {
             testAvailable && (
               <center>
