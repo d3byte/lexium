d44a24730cee653d8b9b9ed3e31c08ad36572e94
Sergey Savtyra <pandinator@protonmail.com>
2018-05-18 21:36:30 +0300

    Сделал скрытие меню на главной странице, если пользователь не авторизован

--- app/src/pages/Main/index.js
+++ app/src/pages/Main/index.js
@@ -4,13 +4,29 @@ import { Link } from 'react-router-dom'
 import Header from '../../components/Header'
 
 import './style.css'
+import { CacheManager } from '../../utils'
 export default class Main extends Component {
+  constructor() {
+    super()
+    this.state = {
+      user: null
+    }
+    this.cache = new CacheManager()
+  }
+
+  componentWillMount = async () => {
+    const user = await this.cache.readData('user')
+    this.setState({ user })
+  }
+  
+
   render() {
+    const { user } = this.state
     const { pathname } = this.props.location
     const { history } = this.props
     return (
       <div className="home-page">
-        <Header pathname={pathname} history={history} />
+        <Header pathname={pathname} history={history} lockMenu={user ? false : true} />
         <div className="section">
           <h1>Учить иностранные языки с Lexium - легко!</h1>
           <p>Просто зайдите в <Link to="/profile">профиль</Link> и попробуйте сами!
